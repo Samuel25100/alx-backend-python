b0e833d31ad66a26a5dc0f1c83ada5806c46e122
Samuel25100 <samuelgselassie1@gmail.com>
2024-08-01 13:42:55 +0300

    add file 1-concat.py

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""concat concatenate two string return that string"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """return str type result"""
+    return "{}{}".format(str1, str2)
