cd179651050536b9349e82423c5873def7f3bd3d
Samuel25100 <samuelgselassie1@gmail.com>
2024-08-01 14:37:36 +0300

    fixing file 6-sum_mixed_list.py

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-"""sum_mixed_list takes a list of integers and floats and returns their sum"""
+"""sum_mixed_list takes a list of integers and floats and returns their sum."""
 from typing import List, Union
 
 
 def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
-    """return float type"""
-    val = 0.0
+    """return float type."""
+    val: float = 0.0
     for i in mxd_lst:
         val += i
     return val
