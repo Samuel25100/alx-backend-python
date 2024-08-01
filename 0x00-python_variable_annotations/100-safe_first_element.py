34205cdbe577381ab8eeb27d40cb3b831cf20a0c
Samuel25100 <samuelgselassie1@gmail.com>
2024-08-01 18:21:28 +0300

    fixing file 100-safe_first_element.py

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -4,6 +4,7 @@ from typing import Sequence, Any, Union
 
 
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """return any or none based on value of lst"""
     if lst:
         return lst[0]
     else:
