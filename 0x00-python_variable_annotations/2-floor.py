8b5ea755e11674f435b0e11b600d2d2fd17358f4
Samuel25100 <samuelgselassie1@gmail.com>
2024-08-01 13:48:34 +0300

    add file 2-floor.py

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""floor round the given arg to lowest integer"""
+import math
+
+
+def floor(n: float) -> int:
+    """return int typed lowest integer of n"""
+    return math.floor(n)
