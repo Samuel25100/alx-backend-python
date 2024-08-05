202313490fc76907890164f2303721bc087167a5
Samuel25100 <samuelgselassie1@gmail.com>
2024-08-05 14:09:46 +0300

    fixing type annotation

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -4,8 +4,8 @@ import random
 import asyncio
 
 
-async def wait_random(max_delay=10):
+async def wait_random(max_delay: int=10) -> float:
     """Val is going to be used for .sleep() input"""
-    val = random.uniform(0, max_delay)
+    val: float = random.uniform(0, max_delay)
     await asyncio.sleep(val)
     return val
--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
 """Spawn n number of wait_random function call"""
 import asyncio
+from typing import List, Any
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n, max_delay):
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """each call run concunrrently"""
-    arr = []
-    result = []
+    arr: List[asyncio.Task]= []
+    result: List[float]= []
     for i in range(0, n):
         tk = asyncio.create_task(wait_random(max_delay))
         arr.append(tk)
     for i in asyncio.as_completed(arr):
-        result.append(await i)
+        got = await i
+        result.append(got)
     return (result)
