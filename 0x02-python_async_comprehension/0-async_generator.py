f26b35f65312044c1340e996257671e8767620f1
Samuel25100 <samuelgselassie1@gmail.com>
2024-08-06 16:44:47 +0300

    fixing file 0-async

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,10 +2,10 @@
 """async_generator function"""
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """asynchronously wait 1 second, then yield a random number"""
     for i in range(10):
         await asyncio.sleep(1)
