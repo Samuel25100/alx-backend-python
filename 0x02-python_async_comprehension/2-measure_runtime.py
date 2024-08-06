b60891b4941585181f7da712da66a7cbdd1591e8
Samuel25100 <samuelgselassie1@gmail.com>
2024-08-06 12:23:04 +0300

    add file 2-measure_runtime.py

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""measure_runtime async function"""
+import asyncio
+from time import perf_counter
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """measure runtime of async_comprehension to run 4 time"""
+    start: float = perf_counter()
+    t1: asyncio.Task = asyncio.create_task(async_comprehension())
+    t2: asyncio.Task = asyncio.create_task(async_comprehension())
+    t3: asyncio.Task = asyncio.create_task(async_comprehension())
+    t4: asyncio.Task = asyncio.create_task(async_comprehension())
+    await asyncio.gather(t1, t2, t3, t4)
+    end: float = perf_counter()
+    return (end - start)
