0fff983ade696431a3a72931b486b06a16755940
Samuel25100 <samuelgselassie1@gmail.com>
2024-08-24 14:36:07 +0300

    add file test_utils.py for task-2

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """test for utils.py"""
 import unittest
+from unittest.mock import patch
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import access_nested_map, get_json
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -24,6 +25,16 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map_exception(self, nest_map, path):
         """check access_nested_map if raise keyerror correctly"""
         self.assertRaises(KeyError, access_nested_map, nest_map, path)
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+        ])
+    @patch('requests.get')
+    def test_get_json(self, url, out, mock_get):
+        """test get_json method that fetch from url"""
+        mock_get.return_value.json.return_value = out
+        self.assertEqual(get_json(url), out)
 
 
 if __name__ == "__main__":
