ff130732ffdc8e52621a1d02228870a077f99f2e
Samuel25100 <samuelgselassie1@gmail.com>
2024-08-25 15:39:00 +0300

    add file test_utils.py for task-3

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,7 +3,7 @@
 import unittest
 from unittest.mock import patch
 from parameterized import parameterized
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -26,7 +26,7 @@ class TestAccessNestedMap(unittest.TestCase):
         """check access_nested_map if raise keyerror correctly"""
         self.assertRaises(KeyError, access_nested_map, nest_map, path)
 
-    
+
 class TestGetJson(unittest.TestCase):
     """utils file get requests test case"""
 
@@ -39,6 +39,25 @@ class TestGetJson(unittest.TestCase):
         """test get_json method that fetch from url"""
         mock_get.return_value.json.return_value = out
         self.assertEqual(get_json(url), out)
+
+
+class TestMemoize(unittest.TestCase):
+    """test memoize"""
+
+    def test_memoize(self):
+        """tests memoize method"""
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+        with patch.object(TestClass, 'a_method') as mock_meth:
+            test_cl = TestClass()
+            test_cl.a_property()
+            test_cl.a_property()
+            mock_meth.assert_called_once()
 
 
 if __name__ == "__main__":
