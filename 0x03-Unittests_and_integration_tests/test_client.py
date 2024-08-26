29e241466cdc94a3d561feca4ecc5b56391e178a
Samuel25100 <samuelgselassie1@gmail.com>
2024-08-26 08:18:49 +0300

    add file test_client.py for task-5

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """test case for client.GithubOrgClient"""
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, PropertyMock
 from parameterized import parameterized
 from client import GithubOrgClient
 from utils import (
@@ -25,6 +25,16 @@ class TestGithubOrgClient(unittest.TestCase):
         val = gitcl.org
         self.assertEqual(val, {"stat": "200"})
         mock_get.assert_called_once()
+
+    def test_public_repos_url(self):
+        """test public_repo"""
+        with patch.object(GithubOrgClient,
+                          'org',
+                          new_callable=PropertyMock) as mock_pub:
+            mock_pub.return_value = {"repos_url": 200}
+            gitcl = GithubOrgClient('google')
+            val = gitcl._public_repos_url
+            self.assertEqual(val, 200)
 
 
 if __name__ == "__main__":
