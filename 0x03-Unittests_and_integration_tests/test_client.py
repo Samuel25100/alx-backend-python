01cbaf89d025bc1ac0493fab975522176df1708e
Samuel25100 <samuelgselassie1@gmail.com>
2024-08-26 09:51:03 +0300

    add file test_client.py for task-6

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -36,6 +36,20 @@ class TestGithubOrgClient(unittest.TestCase):
             val = gitcl._public_repos_url
             self.assertEqual(val, 200)
 
+    @patch('client.get_json')
+    def test_public_repos(self, mock_get):
+        """test public_repos"""
+        mock_get.return_value = [{"name": "repo_name1"},
+                                 {"name": "repo_name2"}]
+        with patch.object(GithubOrgClient, '_public_repos_url') as mock_pub:
+            gitcl = GithubOrgClient('google')
+            mock_pub.return_value = 200
+            val = gitcl.public_repos()
+            url = gitcl._public_repos_url()
+            self.assertEqual(val, ["repo_name1", "repo_name2"])
+            mock_get.assert_called_once()
+            mock_pub.assert_called_once()
+
 
 if __name__ == "__main__":
     unittest.main()
